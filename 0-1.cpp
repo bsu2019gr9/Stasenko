324f378dc6d35b9268e932c9f1064583f13ce01f
Shella202 <42816871+Shella202@users.noreply.github.com>
2020-02-14 22:17:14 +0300

    updated

--- 0-1.cpp
+++ 0-1.cpp
@@ -5,48 +5,43 @@ a=-1,b=1;
 h=0.2
 */
 
-
-
-#define _USE_MATH_DEFINES
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 
 double Exp(double x,double eps) {
 
 	double total_exp=1, curr=1;
-	for (size_t i = 1;fabs(curr) > eps; i++)
+	size_t i = 1;
+	while (fabs(curr) > eps)
 	{
 		curr *= x / i;
 		total_exp += curr;
+		++i;
 	}
 	return total_exp;
 }
 
 double Sin(double x, double eps) {
 	double total_exp = x, curr = x;
-	for (size_t i = 2;fabs(curr) > eps; i+=2)
+	size_t i = 2;
+	while(fabs(curr) > eps)
 	{
 		curr *= -x*x / (i*(i+1));
 		total_exp += curr;
+		i += 2;
 	}
 	return total_exp;
 }
 
 int main() {
 
-	const double exp_2 = M_E * M_E;
 	double a = -1, b = 1, eps = 0.0001, h = 0.2;
 	for (double x = a;x <= b;x += h) {
-		cout << "x=";
-		cout.width(2);
-		cout << x;
-		cout << "\t\tf(x)=";
-		cout.width(6);
-		cout << Exp(x,eps)*exp_2*Sin(2*x,eps);
-		cout << "\t\tF(x)=";
-		cout.width(6);
-		cout << exp(x) * exp_2 * sin(2 * x);
+		cout << "x="<<setw(4)<<x;
+		cout << "\t\tf(x)="<<setw(6)<< Exp(x+2, eps)* Sin(2 * x, eps);
+		cout << "\t\tF(x)="<<exp(x+2)*sin(2*x);
 		cout << "\n";
 	}
 
